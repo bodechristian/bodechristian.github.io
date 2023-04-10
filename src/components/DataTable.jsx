f3774607ef3f352008621e2b30fc6f81e5bd9b41
Christian Bode <christianbode97@web.de>
2023-04-10 15:59:19 +0200

    cleaned up talbe a bit and added sorting

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
-const DataTable = (props) => {
-    var name = Object.keys(props.data)[0];
-    var runs = Object.values(props.data)[0];
+const DataTable = ({data}) => {
+    const [tableData, setTableData] = useState([]);
+    const [sortField, setSortField] = useState("");
+    const [order, setOrder] = useState("asc");
+
+    var name = Object.keys(data)[0];
+    var runs = Object.values(data)[0];
+
+    useEffect(() => setTableData(runs), []);
 
     const columns = [
         { label: "Map", accessor: "map_name" },
@@ -11,23 +19,44 @@ const DataTable = (props) => {
         { label: "Points", accessor: "points" },
        ];
 
+    const handleSortingChange = (accessor) => {
+        const sortOrder =
+            accessor === sortField && order === "asc" ? "desc" : "asc";
+        setSortField(accessor);
+        setOrder(sortOrder);
+        handleSorting(accessor, sortOrder);
+    };
+
+    const handleSorting = (sortField, sortOrder) => {
+        if (sortField) {
+         const sorted = [...tableData].sort((a, b) => {
+          return (
+           a[sortField].toString().localeCompare(b[sortField].toString(), "en", {
+            numeric: true,
+           }) * (sortOrder === "asc" ? 1 : -1)
+          );
+         });
+         setTableData(sorted);
+        }
+       };
+
     return (
         <table>
             <thead>
                 <tr>
                 {columns.map(({ label, accessor }) => {
-                return <th key={accessor}>{label}</th>;
+                return <th key={accessor} onClick={() => handleSortingChange(accessor)}>{label}</th>;
                 })}
                 </tr>
             </thead>
             <tbody>
-                {runs.map((runs, i) => {
+                {tableData.map((runs, i) => {
                     return (
                     <tr key={i}>
-                    {columns.map(({ accessor }) => {
-                    const tData = runs[accessor] ? runs[accessor] : "——";
-                    return <td key={accessor}>{tData}</td>;
-                    })}
+                        {columns.map(({ accessor }) => {
+                        const tData = runs[accessor] ? runs[accessor] : "——";
+                        return <td key={accessor}>{tData}</td>;
+                        })}
                     </tr>
                     );
                 })}
