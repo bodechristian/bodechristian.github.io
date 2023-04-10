0fb6a02b2b4540c4b4c2ce88bef1bc96b529141a
Christian Bode <christianbode97@web.de>
2023-04-10 18:11:37 +0200

    added text field for steamid and mapped ids to names

--- src/pages/KZTourney.jsx
+++ src/pages/KZTourney.jsx
@@ -7,8 +7,11 @@ const KZTourney = () => {
                 "kz_bacho", "kz_dam", "kz_summercliff2_go", "kz_cdr_myst", "kz_hellinashop",
                 "kz_arrebol", "kz_megalodon", "kz_camembert", "kz_module", "kz_forchi",
                 "kz_dzy_reach_v2", "kz_haste", "kz_shark_gc", "kz_lume", "kz_mandelbrot",
-                "kz_okaychamp", "kzpro_gull_pidr_reborn", "kz_hitech", "kz_techtonic_v2_ldr", "bkz_apricity_v3"]
+                "kz_okaychamp", "kzpro_gull_pidr_reborn", "kz_hitech", "kz_techtonic_v2_ldr", "bkz_apricity_v3"];
+    
     const [times, setTimes] = useState([]); // [{name1: [{map:kz_a, time:5...}, {...}, ...]}, {name2: ...}, ...]
+    const [playername, setPlayername] = useState("STEAM_1:0:18890328");
+    const [mapIDName, setMapIDName] = useState({});
 
     function convTime(time) {        
         var m = 0;
@@ -17,9 +20,28 @@ const KZTourney = () => {
         return m===0 ? time : String(m) + ":" + ('000000'+s).slice(-6);
     }
 
-    function loadTimes() {
-        var player_id = "STEAM_1:0:18890328"
-        var requestURL = 'https://kztimerglobal.com/api/v2/records/top/recent?steam_id='+player_id+'&has_teleports=false&tickrate=128&stage=0&modes_list_string=kz_timer&limit=1000';
+    function addPlayer() {
+        if(playername.startsWith("STEAM") && !Object.keys(mapIDName).includes(playername)) {
+            var requestURL = "https://kztimerglobal.com/api/v2/players?steam_id=" + playername
+            fetch(requestURL)
+                .then(response => response.json())
+                .then(data => setMapIDName({...mapIDName, [playername]:data[0]["name"]}))
+                .then(() => loadTimes(playername));
+        } else if(!Object.values(mapIDName).includes(playername) && playername) {
+            // DOESNT WORK WELL... USE STEAM ID
+            return ;
+            var requestURL = "https://kztimerglobal.com/api/v2/players?name=" + playername
+            fetch(requestURL)
+                .then(response => response.json())
+                .then(data => {setMapIDName({...mapIDName, [data[0]["steam_id"]]:data[0]["name"]}); return data;})     
+                .then(data => loadTimes(data[0]["steam_id"]));   
+        } else {
+            return ;
+        }
+    }
+    
+    function loadTimes(id) {
+        var requestURL = 'https://kztimerglobal.com/api/v2/records/top/recent?steam_id='+id+'&has_teleports=false&tickrate=128&stage=0&modes_list_string=kz_timer&limit=1000';
         fetch(requestURL)
             .then(response => response.json())
             .then(data => data.filter(el => maps.includes(el["map_name"])))
@@ -27,18 +49,22 @@ const KZTourney = () => {
                 var tier = Math.floor(maps.indexOf(el["map_name"])/5)+1;
                 var time = convTime(el["time"]);
                 return {...el, tier:tier, time:time}}))
-            .then(data => setTimes([...times, {[player_id]: data}]));
+            .then(data => setTimes([...times, {[id]: data}]));
         return requestURL;
     }
 
     return (
         <div>
             <h3>KZ Tourney</h3>
-            <button onClick={() => loadTimes()}>Load</button>
+            <div className='row_addplayer form-inline'>
+                <input className='form-control' type="text" value={playername}
+                        onChange={(a) => setPlayername(a.target.value)} placeholder="STEAM_1:0:18890328"/>
+                <button className='btn-primary btn' onClick={() => addPlayer()}>Add</button>
+            </div>
             <div className='tables_container'>
                 {times.map(player_data => (
                     <div className="table_container">
-                        <h5>{Object.keys(player_data)[0]}</h5>
+                        <h5>{mapIDName[Object.keys(player_data)[0]]}</h5>
                         <DataTable data={player_data}/>
                     </div>
                 ))}
